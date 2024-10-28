6fbf3c7d93f65d11f677b5dbce25381ec90ffef2
Soumyadip Ojha <soumyadipojha635@gmail.com>
2024-10-28 10:12:04 +0530

    solving cors error

--- server.js
+++ server.js
@@ -30,18 +30,20 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(
-  cors({
-    origin: "https://shalinisaree.netlify.app",
-    methods: ["GET", "POST", "DELETE", "PUT"],
-    allowedHeaders: [
-      "Content-Type",
-      "Authorization",
-      "Cache-Control",
-      "Expires",
-      "Pragma",
-    ],
-    credentials: true,
-  })
+  cors(
+    // {
+  //   origin: "https://shalinisaree.netlify.app",
+  //   methods: ["GET", "POST", "DELETE", "PUT"],
+  //   allowedHeaders: [
+  //     "Content-Type",
+  //     "Authorization",
+  //     "Cache-Control",
+  //     "Expires",
+  //     "Pragma",
+  //   ],
+  //   credentials: true,
+  // }
+)
 );
 
 app.use(cookieParser());
