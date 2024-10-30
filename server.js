cb6061b631dc2b06f0cc056c46e69901f466498f
Soumyadip Ojha <soumyadipojha635@gmail.com>
2024-10-31 00:25:56 +0530

    image upload

--- server.js
+++ server.js
@@ -2,57 +2,58 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
-const helmet = require("helmet");
-require("dotenv").config(); // Ensure .env variables are loaded
-
 const authRouter = require("./routes/auth/auth-routes");
 const adminProductsRouter = require("./routes/admin/products-routes");
 const adminOrderRouter = require("./routes/admin/order-routes");
+
 const shopProductsRouter = require("./routes/shop/products-routes");
 const shopCartRouter = require("./routes/shop/cart-routes");
 const shopAddressRouter = require("./routes/shop/address-routes");
 const shopOrderRouter = require("./routes/shop/order-routes");
 const shopSearchRouter = require("./routes/shop/search-routes");
 const shopReviewRouter = require("./routes/shop/review-routes");
+
 const commonFeatureRouter = require("./routes/common/feature-routes");
 
-// Create a database connection
+//create a database connection -> u can also
+//create a separate file for this and then import/use that file here
+
 mongoose
-  .connect("mongodb+srv://shashusee234:199oENKi1pAIiDqQ@cluster0.zkbh6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+  .connect("mongodb://127.0.0.1:27017/sareeeee")
   .then(() => console.log("MongoDB connected"))
-  .catch((error) => console.error("MongoDB connection error:", error));
+  .catch((error) => console.log(error));
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Updated CORS Configuration
 app.use(
   cors({
-    origin: "https://shalinisaree.netlify.app/", // Specify the exact origin
-    credentials: true, // Allow cookies and auth headers
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"], // Allow necessary headers
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "DELETE", "PUT"],
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "Cache-Control",
+      "Expires",
+      "Pragma",
+    ],
+    credentials: true,
   })
 );
 
-app.use(helmet());
-app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
-
-// Middleware
 app.use(cookieParser());
 app.use(express.json());
-
-// Route Handlers
 app.use("/api/auth", authRouter);
 app.use("/api/admin/products", adminProductsRouter);
 app.use("/api/admin/orders", adminOrderRouter);
+
 app.use("/api/shop/products", shopProductsRouter);
 app.use("/api/shop/cart", shopCartRouter);
 app.use("/api/shop/address", shopAddressRouter);
 app.use("/api/shop/order", shopOrderRouter);
 app.use("/api/shop/search", shopSearchRouter);
 app.use("/api/shop/review", shopReviewRouter);
+
 app.use("/api/common/feature", commonFeatureRouter);
 
-// Start Server
 app.listen(PORT, () => console.log(`Server is now running on port ${PORT}`));
